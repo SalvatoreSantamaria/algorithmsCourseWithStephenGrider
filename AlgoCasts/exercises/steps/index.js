37d84b64082cd5e5602d9a9c48a1de8abde9b327
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-05-11 07:31:52 -0700

    Steps

--- AlgoCasts/exercises/steps/index.js
+++ AlgoCasts/exercises/steps/index.js
@@ -17,6 +17,20 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let row = 0; row < n; row++) {   //iterate thru rows
+    let stair = '';
+    for (let column = 0; column < n; column++){ //iterate thru columns
+      if (column <= row) { //if current column less than or equal to row
+        stair += '#'; //add #
+      } else {
+        stair += ' ';  //add space
+      }
+    }
+    console.log(stair) //after we have processed entire row, make sure to console.log the row. (ie, the full stair string.)
+  }
+
+
+}
 
 module.exports = steps;
