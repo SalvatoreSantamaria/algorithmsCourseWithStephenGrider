0092d4c5d6d04ca555faa74edbec215892a901e0
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-10-31 07:22:44 -0700

    Added insert first

--- AlgoCasts/exercises/linkedlist/index.js
+++ AlgoCasts/exercises/linkedlist/index.js
@@ -9,6 +9,14 @@ class Node {
   }
 }
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+// in practice:
+// const list = new LinkedList(); //create new LinkedList, head is null
+// list.head = new Node(10) //set the head to have a node with data:10 and of course next = null
 
 module.exports = { Node, LinkedList };
